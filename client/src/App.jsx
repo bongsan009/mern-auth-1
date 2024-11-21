c427d3b367d5f6bdc5e40ff59efcc11379ac3c39
OeunBunsan <bbek2930@gmail.com>
2024-11-21 11:28:12 +0700

    Install react js tailwind css and the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className= "text-red-100">
+        This is the fist project that i built with react vite
+    </div>
+  )
+}
