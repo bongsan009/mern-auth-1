150ab3aa12b6af73492837c79ff1aab5a1a676c3
OeunBunsan <bbek2930@gmail.com>
2024-11-21 13:20:15 +0700

    connect to DB

--- api/index.js
+++ api/index.js
@@ -3,6 +3,18 @@
 
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected successfully");
+}).catch((e) => {
+    console.log(e)
+})
+
 
 const app =  new express();
 
